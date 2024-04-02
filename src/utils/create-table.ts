b9a8b0f26f29d2eb5534c2de4e757118fe091b8b
flowcore-platform <gitops@flowcore.io>
2024-04-02 17:08:37 +0100

    fix: upgrades decimal type to double if it exceeds the decimal limit

--- src/utils/create-table.ts
+++ src/utils/create-table.ts
@@ -3,23 +3,23 @@ import { db } from "../db";
 import { Knex } from "knex";
 
 const COLUMN_FACTORY: TableMapper = {
-  "text": (table: Knex.CreateTableBuilder, key: string) => table.text(key),
-  "string": (table: Knex.CreateTableBuilder, key: string) => table.string(key),
-  "integer": (table: Knex.CreateTableBuilder, key: string) => table.integer(key),
-  "decimal": (table: Knex.CreateTableBuilder, key: string) => table.decimal(key),
-  "boolean": (table: Knex.CreateTableBuilder, key: string) => table.boolean(key),
-  "json": (table: Knex.CreateTableBuilder, key: string) => table.json(key),
-  "jsonb": (table: Knex.CreateTableBuilder, key: string) => table.jsonb(key),
-  "uuid": (table: Knex.CreateTableBuilder, key: string) => table.uuid(key),
-  "timestamp": (table: Knex.CreateTableBuilder, key: string) => table.timestamp(key),
-  "binary": (table: Knex.CreateTableBuilder, key: string) => table.binary(key),
-  "bigInteger": (table: Knex.CreateTableBuilder, key: string) => table.bigInteger(key),
-  "float": (table: Knex.CreateTableBuilder, key: string) => table.float(key),
-  "double": (table: Knex.CreateTableBuilder, key: string) => table.double(key),
-  "increments": (table: Knex.CreateTableBuilder, key: string) => table.increments(key),
-  "time": (table: Knex.CreateTableBuilder, key: string) => table.time(key),
-  "date": (table: Knex.CreateTableBuilder, key: string) => table.date(key),
-  "dateTime": (table: Knex.CreateTableBuilder, key: string) => table.dateTime(key),
+  "text": (table: Knex.CreateTableBuilder, key: string) => table.text(key).nullable(),
+  "string": (table: Knex.CreateTableBuilder, key: string) => table.string(key).nullable(),
+  "integer": (table: Knex.CreateTableBuilder, key: string) => table.integer(key).nullable(),
+  "decimal": (table: Knex.CreateTableBuilder, key: string) => table.decimal(key).nullable(),
+  "boolean": (table: Knex.CreateTableBuilder, key: string) => table.boolean(key).nullable(),
+  "json": (table: Knex.CreateTableBuilder, key: string) => table.json(key).nullable(),
+  "jsonb": (table: Knex.CreateTableBuilder, key: string) => table.jsonb(key).nullable(),
+  "uuid": (table: Knex.CreateTableBuilder, key: string) => table.uuid(key).nullable(),
+  "timestamp": (table: Knex.CreateTableBuilder, key: string) => table.timestamp(key).nullable(),
+  "binary": (table: Knex.CreateTableBuilder, key: string) => table.binary(key).nullable(),
+  "bigInteger": (table: Knex.CreateTableBuilder, key: string) => table.bigInteger(key).nullable(),
+  "float": (table: Knex.CreateTableBuilder, key: string) => table.float(key).nullable(),
+  "double": (table: Knex.CreateTableBuilder, key: string) => table.double(key).nullable(),
+  "increments": (table: Knex.CreateTableBuilder, key: string) => table.increments(key).nullable(),
+  "time": (table: Knex.CreateTableBuilder, key: string) => table.time(key).nullable(),
+  "date": (table: Knex.CreateTableBuilder, key: string) => table.date(key).nullable(),
+  "dateTime": (table: Knex.CreateTableBuilder, key: string) => table.dateTime(key).nullable(),
 };
 
 export async function createTable(tableName: string, schema: TableSchema) {
@@ -36,7 +36,7 @@ export async function createTable(tableName: string, schema: TableSchema) {
         console.error(`Unknown type ${value.type} for key ${key}`);
         continue;
       }
-      
+
       factory(table, key);
     }
   });
